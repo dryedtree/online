72cb657c26d19395206788858b0ee466cc8f452d
dryed tree <167512272+dryedtree@users.noreply.github.com>
2025-12-16 12:53:24 +0900

    Update render.js

--- docs/render.js
+++ docs/render.js
@@ -1,23 +1,6 @@
 function render(ctx, game) {
   ctx.clearRect(0, 0, 800, 600);
 
-  game.effects.forEach(e => {
-    ctx.globalAlpha = e.life / 15;
-    ctx.strokeStyle = "yellow";
-    ctx.beginPath();
-    ctx.arc(e.x, e.y, e.r++, 0, Math.PI * 2);
-    ctx.stroke();
-    ctx.globalAlpha = 1;
-  });
-
-  ctx.fillStyle = "cyan";
-  ctx.beginPath();
-  ctx.arc(game.player.x, game.player.y, 10, 0, Math.PI * 2);
-  ctx.fill();
-}
-function render(ctx, game) {
-  ctx.clearRect(0, 0, 800, 600);
-
   // 敵
   ctx.fillStyle = "red";
   game.enemies.forEach(e => {
